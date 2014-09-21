6508a0731e7d6586847b8d7e87bc45e11c3c2de3
yahor-filipchyk <yahor.filipchyk@gmail.com>
2014-09-21 20:57:57 +0300

    Typo in key

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.yahor.clojure/fplabs "1.0.0-SNAPSHOT"
   :description "Project for Functional programming tasks"
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :resource-path ["resources"]
+  :resource-paths ["resources"]
   :aot :all
   :main org.yahor.clojure.fplabs.core)
